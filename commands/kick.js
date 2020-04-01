916ad21b26d7c283c164a3d8e51ac0b1aeba4272
Valentine Briese <controlfreak707@gmail.com>
2020-04-01 16:48:56 -0700

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "kick",
   description: "Kicks the specified user for a specified reason.",
   execute(message, args) {
-    if (message.author.hasPermission("KICK_MEMBERS")) {
+    if (message.member.hasPermission("KICK_MEMBERS")) {
       const user = message.mentions.users.first();
       if (user) {
         const member = message.guild.member(user);
