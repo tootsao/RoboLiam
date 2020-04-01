64ed0d94df5fb1b9f25e52020fd43dd2d4c5645b
Valentine Briese <controlfreak707@gmail.com>
2020-04-01 16:39:54 -0700

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,24 +2,30 @@ module.exports = {
   name: "kick",
   description: "Kicks the specified user for a specified reason.",
   execute(message, args) {
-    const user = message.mentions.users.first();
-    if (user) {
-      const member = message.guild.member(user);
-      if (member) {
-        member
-          .kick()
-          .then(() => {
-            message.reply(`Succesfuly kicked ${user.tag}`);
-          })
-          .catch(err => {
-            message.reply("I was unable to kick the user you requested.");
-            console.log(err);
-          });
+    if (message.author.roles.cache.has("533712722311905290") === true) {
+      const user = message.mentions.users.first();
+      if (user) {
+        const member = message.guild.member(user);
+        if (member) {
+          member
+            .kick()
+            .then(() => {
+              message.reply(`Succesfuly kicked ${user.tag}`);
+            })
+            .catch(err => {
+              message.reply("I was unable to kick the user you requested.");
+              console.log(err);
+            });
+        } else {
+          message.reply(
+            `The user you requested isn\'t a member of this server.`
+          );
+        }
       } else {
-        message.reply(`The user you requested isn\'t a member of this server.`);
+        message.reply("Couldn't find the user you requested!");
       }
     } else {
-      message.reply("Couldn't find the user you requested!");
+      message.channel.send("Insuficent permmisions.");
     }
   }
 };
