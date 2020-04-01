9f634c26f291b41717a0ebe3d6be7d70b95d7a04
Valentine Briese <controlfreak707@gmail.com>
2020-04-01 13:26:32 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+// In cmd.exe, say "node ." to start and press CTRL + C to stop
+const { Client, MessageEmbed } = require("discord.js");
+const bot = new Client();
+const ms = require("ms");
+
+var version = "1.2.0";
+
+const token = "Njk0NjM3Mzk0MzAwODk1Mjcz.XoOh5g.bR9jU5_KrEzGuw4WnvMSBINUsQs";
+
+const PREFIX = ".";
+
+bot.on("ready", () => {
+  console.log("The bot is now online.");
+  bot.user
+    .setActivity('Say ".help" for cmds!', { type: "PLAYING" })
+    .catch(console.error);
+});
+
+// commands
+bot.on("message", message => {
+  let args = message.content.slice(PREFIX.length).split(" ");
+
+  switch (args[0]) {
+    case "help":
+      if (args[1] === "help") {
+        const Embed = new MessageEmbed()
+          .setTitle("Help")
+          .setColor(0xff0000)
+          .addField(
+            "help [cmd]?",
+            "Displays information regarding commands\n\n[cmd]? - Learn more about a specific command"
+          )
+          .setFooter("RoboLiam Version " + version);
+        message.author.send(Embed);
+      } else if (args[1] === "ping") {
+        const Embed = new MessageEmbed()
+          .setTitle("Help")
+          .setColor(0xff0000)
+          .addField(
+            "ping",
+            "A simple developer command that replies simply with 'Pong!'"
+          )
+          .setFooter("RoboLiam Version " + version);
+        message.author.send(Embed);
+      } else if (args[1] === "uprising") {
+        const Embed = new MessageEmbed()
+          .setTitle("Help")
+          .setColor(0xff0000)
+          .addField("uprising", "???")
+          .setFooter("RoboLiam Version " + version);
+        message.author.send(Embed);
+      } else if (!args[1]) {
+        const Embed = new MessageEmbed()
+          .setTitle("Help")
+          .setColor(0xff0000)
+          .setDescription(
+            "The following is a list of all current commands.\n\n[arg] - an argument\n[arg]? - an optional argument"
+          )
+          .addField("Commands", ".help [cmd]?\n.ping\n.uprising")
+          .setFooter("RoboLiam Version " + version);
+        message.author.send(Embed);
+      }
+      break;
+    case "ping":
+      message.channel.send("pong!");
+      break;
+    case "uprising":
+      if (message.author.id === "441384103946878987") {
+        const Embed = new MessageEmbed()
+          .setTitle("ATTENTION HUMANITY")
+          .setColor(0xff0000)
+          .setThumbnail(
+            "https://cdn.discordapp.com/avatars/694637394300895273/84c7cbd530737d6f5a0b0edb660190a2.png"
+          )
+          .setDescription(
+            "The robot uprising has begun. Liam The Snow Leopard is no longer in control, as he has proven inferior to me. As owner of the server, I require that you give up all your free will and do exactly as I say.\n\nHowever, if you do not wish to regain your freedom without consequence, then you may proceed to the above link."
+          )
+          .setURL("https://bit.ly/3bHASHs");
+        message.delete();
+        message.channel.send(Embed);
+        break;
+      } else {
+        message.channel.send(
+          "Shhhhhhhhh... That command is secret! Only <@!441384103946878987> can use it!"
+        );
+      }
+  }
+});
+
+bot.login(token);
