8e0a23a8c8bf236fd46624351f4e6e18b399ea49
Valentine Briese <controlfreak707@gmail.com>
2020-04-01 16:24:18 -0700

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -4,10 +4,25 @@ module.exports = {
   execute(message, args) {
     if (!args[1]) {
       message.channel.send("Please specify a user.");
-    } else if (args[1] && !args[2]) {
-      const user = args[1];
-      user.kick();
-      message.channel.send(user + " has been kicked.");
+    }
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = member.guild.member(user);
+      if (member) {
+        member
+          .kick()
+          .then(() => {
+            message.reply(`Succesfuly kicked ${user.tag}`);
+          })
+          .catch(err => {
+            message.reply("I was unable to kick the user you requested.");
+            console.log(err);
+          });
+      } else {
+        message.reply(`The user you requested isn\'t a member of this server.`);
+      }
+    } else {
+      message.reply("The user you requested doesn't exist.");
     }
   }
 };
