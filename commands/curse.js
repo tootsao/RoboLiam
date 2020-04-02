4d2b32cdf817d1096a758dae0f41e51ab877892e
Valentine Briese <controlfreak707@gmail.com>
2020-04-01 20:59:56 -0700

    Update curse.js

--- commands/curse.js
+++ commands/curse.js
@@ -8,6 +8,16 @@ module.exports = {
           message.author.id +
           ">, you didn't tell me who you wanted to curse!"
       );
+    } else {
+      const user = message.mentions.users.first();
+      if (user) {
+        const member = message.guild.member(user);
+        if (member) {
+          message.reply("<@!" + member.id + "> has been cursed! :smiling_imp:");
+        } else {
+          message.channel.send("The user you requested isn't on this server.");
+        }
+      }
     }
   }
 };
