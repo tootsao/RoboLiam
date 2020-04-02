3e2ee0a4383ac1a068d716bde5c98bb2d8a3a999
Valentine Briese <controlfreak707@gmail.com>
2020-04-01 20:19:10 -0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,19 +3,26 @@ module.exports = {
   name: "poll",
   description: "Creates a poll.",
   execute(message, args) {
-    if (!args[1]) {
-      message.channel.send("Invalid Arguments.");
+    if (
+      message.member.roles.cache.has("533712722311905290") ||
+      message.member.roles.cache.has("450746674202017792")
+    ) {
+      if (!args[1]) {
+        message.channel.send("Invalid Arguments.");
+      } else {
+        let msgArgs = args.slice(1).join(" ");
+        const Embed = new MessageEmbed()
+          .setColor(0xffc300)
+          .setTitle("Poll")
+          .setDescription(msgArgs);
+        message.channel.send(Embed).then(messageReaction => {
+          messageReaction.react("👍");
+          messageReaction.react("👎");
+          message.delete({ timeout: 500 }).catch(console.error);
+        });
+      }
     } else {
-      let msgArgs = args.slice(1).join(" ");
-      const Embed = new MessageEmbed()
-        .setColor(0xffc300)
-        .setTitle("Poll")
-        .setDescription(msgArgs);
-      message.channel.send(Embed).then(messageReaction => {
-        messageReaction.react("👍");
-        messageReaction.react("👎");
-        message.delete({ timeout: 500 }).catch(console.error);
-      });
+      message.channel.send("Insuficent permmisions.");
     }
   }
 };
