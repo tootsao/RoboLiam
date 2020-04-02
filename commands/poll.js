d7ae8950d83993c4af652017d6925d8bd6990bb5
Valentine Briese <controlfreak707@gmail.com>
2020-04-01 19:41:39 -0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -14,7 +14,7 @@ module.exports = {
       message.channel.send(Embed).then(messageReaction => {
         messageReaction.react("👍");
         messageReaction.react("👎");
-        message.delete(1000).catch(console.error);
+        message.delete({ timeout: 1000 }).catch(console.error);
       });
     }
   }
