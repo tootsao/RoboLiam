5a225895981a9ba69e095931b4b2a20659138ea4
Valentine Briese <controlfreak707@gmail.com>
2020-04-02 16:07:17 -0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -13,7 +13,7 @@ module.exports = {
         let msgArgs = args.slice(1).join(" ");
         const Embed = new MessageEmbed()
           .setColor(0xffc300)
-          .setTitle("📋 Poll - By @" + message.author.tag)
+          .setTitle("📋 Poll - By @" + message.author.nickname)
           .setDescription(msgArgs)
           .addField("Key", "👍 = Yes!\n👎 = No!");
         message.channel.send(Embed).then(messageReaction => {
