98cad79adf10b3985b03f658acb8b82effc46730
Valentine Briese <controlfreak707@gmail.com>
2020-04-02 17:44:18 -0700

    Update secret.js

--- commands/secret.js
+++ commands/secret.js
@@ -2,14 +2,14 @@ module.exports = {
   name: "secret",
   description: "Congratulates the user for finding a secret.",
   execute(message, args) {
+    const attachment = new attachment(
+      "https://vignette.wikia.nocookie.net/club-penguin-rewritten/images/c/ce/You_Found_A_Secret.png"
+    );
     message.channel.send(
       "***WOOP!***\nHey @everyone, <@!" +
         message.author.id +
         "> has found a secret! :D",
-      {
-        file:
-          "https://vignette.wikia.nocookie.net/club-penguin-rewritten/images/c/ce/You_Found_A_Secret.png"
-      }
+      attachment
     );
   }
 };
