529649e8d390b940e0222e2d2d3128ee24d77dab
Valentine Briese <controlfreak707@gmail.com>
2020-04-02 18:22:52 -0700

    Update secret.js

--- commands/secret.js
+++ commands/secret.js
@@ -3,12 +3,19 @@ module.exports = {
   name: "secret",
   description: "Congratulates the user for finding a secret.",
   execute(message, args) {
+    if (message.guild.id === "450114903865294862") {
+      var mention = "<&506971411336921098>";
+    } else {
+      var mention = "everyone";
+    }
     const attachment = new MessageAttachment(
       "https://vignette.wikia.nocookie.net/club-penguin-rewritten/images/c/ce/You_Found_A_Secret.png"
     );
     message.channel
       .send(
-        "***WOOP!***\nHey @everyone, <@!" +
+        "***WOOP!***\nHey " +
+          mention +
+          ", <@!" +
           message.author.id +
           "> found the secret command! 😃",
         attachment
