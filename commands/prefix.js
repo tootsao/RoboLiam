aa475e159dc0b467a674a2b75d7d44feeb8ed1bf
Valentine Briese <controlfreak707@gmail.com>
2020-04-03 12:05:56 -0700

    Make small changes

--- commands/prefix.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-module.exports = {
-  name: "prefix",
-  description: "Sets prefix.",
-  execute(message, args) {
-    message.channel.send("Sorry, that feature is not yet implemented!");
-  }
-};
--- commands/setPrefix.js
+++ commands/setPrefix.js
@@ -2,6 +2,8 @@ module.exports = {
   name: "setPrefix",
   description: "Sets server prefix",
   execute(message, args, db) {
+    if (!message.member.hasPermission("MANAGE_CHANNELS")) return;
+
     let nPrefix = args[1];
 
     db.collection("guilds")
