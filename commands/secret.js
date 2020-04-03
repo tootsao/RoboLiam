b9797d8ca9f48abe31f9fad25312141fcdcfd9ee
Valentine Briese <controlfreak707@gmail.com>
2020-04-02 17:57:43 -0700

    Update secret.js

--- commands/secret.js
+++ commands/secret.js
@@ -1,10 +1,9 @@
-Discord = require("discord.js");
-const { Client, Attachment } = require("discord.js");
+const { MessageAttachment } = require("discord.js");
 module.exports = {
   name: "secret",
   description: "Congratulates the user for finding a secret.",
   execute(message, args) {
-    const attachment = new Attachment(
+    const attachment = new MessageAttachment(
       "https://vignette.wikia.nocookie.net/club-penguin-rewritten/images/c/ce/You_Found_A_Secret.png"
     );
     message.channel.send(
