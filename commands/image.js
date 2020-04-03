947aef947e8480c09e48c2bc968bb7dbadb2104a
Valentine Briese <controlfreak707@gmail.com>
2020-04-03 13:18:51 -0700

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -5,34 +5,34 @@ module.exports = {
   name: "image",
   description: "Sends an image of choice.",
   execute(message, args) {
+    function image(message) {
+      var options = {
+        url: "https://results.dogpile.com/serp?qc=images&q=" + args[1],
+        method: "GET",
+        headers: {
+          Accept: "text/html",
+          "User-Agent": "Chrome"
+        }
+      };
+
+      request(options, function(error, response, responseBody) {
+        if (error) {
+          return;
+        }
+        $ = cheerio.load(responseBody);
+        var links = $(".image a.link");
+        var urls = new Array(links.length)
+          .fill(0)
+          .map((v, i) => links.eq(i).attr("href"));
+        console.log(urls);
+        if (!urls.length) {
+          return;
+        }
+        //send result
+        message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
+      });
+    }
+
     image(message);
   }
 };
-
-function image(message) {
-  var options = {
-    url: "https://results.dogpile.com/serp?qc=images&q=" + args[1],
-    method: "GET",
-    headers: {
-      Accept: "text/html",
-      "User-Agent": "Chrome"
-    }
-  };
-
-  request(options, function(error, response, responseBody) {
-    if (error) {
-      return;
-    }
-    $ = cheerio.load(responseBody);
-    var links = $(".image a.link");
-    var urls = new Array(links.length)
-      .fill(0)
-      .map((v, i) => links.eq(i).attr("href"));
-    console.log(urls);
-    if (!urls.length) {
-      return;
-    }
-    //send result
-    message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
-  });
-}
