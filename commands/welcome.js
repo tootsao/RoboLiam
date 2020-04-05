0ab87436e83dd60678088c08ec5b545483982c64
Valentine Briese <controlfreak707@gmail.com>
2020-04-05 12:12:50 -0700

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -5,7 +5,11 @@ module.exports = {
   execute(message, args) {
     const Embed = new MessageEmbed()
       .setTitle("Welcome to RoboLiam Paradise!")
-      .setColor(0x0087b1);
+      .setColor(0x0087b1)
+      .setDescription("")
+      .setImage(
+        "https://repository-images.githubusercontent.com/252278043/e73c6000-742f-11ea-9bb2-3744210750be"
+      );
     message.channel.send(Embed);
   },
 };
