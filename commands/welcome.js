a8ff2ede1cd6d79af27b1f05cc246f227026d102
Valentine Briese <controlfreak707@gmail.com>
2020-04-05 12:18:24 -0700

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -7,7 +7,7 @@ module.exports = {
       .setTitle("Welcome to RoboLiam Paradise!")
       .setColor(0x0087b1)
       .setDescription(
-        "Welcome to **RoboLiam Paradise**! This server is is entirely based around Liam's bot, **RoboLiam**. The server is more or lesss just a place for Liam to test the bot, so there's not much here. However, if you want, this is the perfect place to test the bot, decide if you'd like it on your own server, report bugs, and make suggestions!"
+        "Welcome to **RoboLiam Paradise**! This server is is entirely based around Liam's bot, **RoboLiam**. The server is more or less just a place for Liam to test the bot, so there's not much here. However, if you want, this is the perfect place to test the bot, decide if you'd like it on your own server, report bugs, and make suggestions!"
       )
       .setImage(
         "https://repository-images.githubusercontent.com/252278043/e73c6000-742f-11ea-9bb2-3744210750be"
