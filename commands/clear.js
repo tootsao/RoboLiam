a1bee7090979979bc3ebe3c9a2c056a98a53890a
Valentine Briese <controlfreak707@gmail.com>
2020-04-08 19:07:18 -0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -8,11 +8,12 @@ module.exports = {
       message.reply("Please specify the amount of messages to clear.");
     } else {
       message.delete().then(() => {
-        channel
+        message.channel
           .bulkDelete(args[1])
           .then((messages) =>
             message.channel.send(`Cleared ${messages.size} messages.`)
-          );
+          )
+          .catch(console.error);
       });
     }
   },
