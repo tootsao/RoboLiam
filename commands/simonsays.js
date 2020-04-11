6501178736e7edf10cdea223f5ba11ee5fac4a90
Valentine Briese <controlfreak707@gmail.com>
2020-04-10 18:33:52 -0700

    Update simonsays.js

--- commands/simonsays.js
+++ commands/simonsays.js
@@ -2,9 +2,13 @@ module.exports = {
   name: "simonSays",
   description: "Responds with whatever the user says.",
   execute(message, args) {
-    let msgArgs = args.slice(1).join(" ");
-    message.channel.send(msgArgs).then(() => {
-      message.delete().catch(console.error);
-    });
-  }
+    if (!args[1]) {
+      message.channel.send("Please specify what to say!");
+    } else {
+      let msgArgs = args.slice(1).join(" ");
+      message.channel.send(msgArgs).then(() => {
+        message.delete().catch(console.error);
+      });
+    }
+  },
 };
