1a1ddf06eadb8bd662d5088972a203dd480789b8
Valentine Briese <controlfreak707@gmail.com>
2020-04-13 15:41:46 -0700

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -3,9 +3,20 @@ module.exports = {
   name: "info",
   description: "Displays bot info.",
   execute(message, args) {
+    let totalSeconds = client.uptime / 1000;
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+
     const Embed = new MessageEmbed()
       .setTitle("Bot Info")
-      .addField("Servers", message.guild.size, true);
+      .addField("Servers", "W.I.P.", true)
+      .addField(
+        "Uptime",
+        `${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`
+      );
 
     message.channel.send(Embed);
   },
