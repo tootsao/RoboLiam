a722421fb06c5d796248517a516434b36d6c5c2d
Valentine Briese <controlfreak707@gmail.com>
2020-04-13 13:43:07 -0700

    Update serverInfo.js

--- commands/serverInfo.js
+++ commands/serverInfo.js
@@ -6,7 +6,8 @@ module.exports = {
     const Embed = new MessageEmbed()
       .setTitle(message.guild.name)
       .setThumbnail(message.guild.iconURL())
-      .addField("Owner", message.guild.owner.user.tag, true);
+      .addField("Owner", message.guild.owner.user.tag, true)
+      .addField("Region", message.guild.region, true);
 
     message.channel.send(Embed);
   },
