0fb68debcd0f2c84cc2dabadf9996cc3e87421ec
Valentine Briese <controlfreak707@gmail.com>
2020-04-13 13:26:55 -0700

    Update serverInfo.js

--- commands/serverInfo.js
+++ commands/serverInfo.js
@@ -3,7 +3,12 @@ module.exports = {
   name: "serverInfo",
   description: "Displays server information.",
   execute(message, args) {
-    const Embed = new MessageEmbed().setTitle(message.guild.name);
+    const Embed = new MessageEmbed()
+      .setTitle(message.guild.name)
+      .addField(
+        "Server icon hash, whatever that means lol",
+        message.guild.icon
+      );
 
     message.channel.send(Embed);
   },
