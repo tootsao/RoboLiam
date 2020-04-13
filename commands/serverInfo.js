c03f243ab79243dcbd42b85e333a1ef52843cfa5
Valentine Briese <controlfreak707@gmail.com>
2020-04-13 13:20:39 -0700

    Update serverInfo.js

--- commands/serverInfo.js
+++ commands/serverInfo.js
@@ -1,5 +1,10 @@
+const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "serverInfo",
   description: "Displays server information.",
-  execute(message, args) {},
+  execute(message, args) {
+    const Embed = new MessageEmbed().setTitle(guild.name);
+
+    message.channel.send(Embed);
+  },
 };
