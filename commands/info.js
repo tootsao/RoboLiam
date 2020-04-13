1401f8dae32ade0de5a4b4156801587b9cb94ee1
Valentine Briese <controlfreak707@gmail.com>
2020-04-13 15:17:04 -0700

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -4,11 +4,6 @@ const { MessageEmbed } = require("discord.js");
 const firebase = require("firebase/app");
 const FieldValue = require("firebase-admin").firestore.FieldValue;
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccount.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
 
 let db = admin.firestore();
 
