4005796e56a4666c7f17b8369f090c10d41732f4
Valentine Briese <controlfreak707@gmail.com>
2020-04-14 15:59:34 -0700

    Update vibeCheck.js

--- commands/vibeCheck.js
+++ commands/vibeCheck.js
@@ -2,20 +2,39 @@ module.exports = {
   name: "vibeCheck",
   description: "V I B E   C H E C K",
   execute(message, args) {
+    const bot = require("../index.js");
+
     if (!args[1]) {
       message.channel.send("Who should I distribute a vibe check to?");
     } else {
-      const mentionedUser = message.mentions.users.first();
-      let target;
-      if (mentionedUser.nickname) {
-        target = `<@!${mentionedUser.id}>`;
+      const target = message.mentions.users.first();
+      let targetMsg;
+      if (target.nickname) {
+        targetMsg = `<@!${target.id}>`;
       } else {
-        target = `<@${mentionedUser.id}>`;
+        targetMsg = `<@${target.id}>`;
       }
 
       message.channel.send(
-        `<@${message.author.id}> has called upon a vibe check...\n***...On ${target}!***\n\n${target}, whatever you say next will be judged! So choose your words carefully...`
+        `<@${message.author.id}> has called upon a vibe check...\n***...On ${targetMsg}!***\n\n${targetMsg}, whatever you say next will be judged! So choose your words carefully...`
       );
+
+      const targetHasResponded = false;
+      while (targetHasResponded == false) {
+        bot.on("message", async (nextMessage) => {
+          if (nextMessage.author == target) {
+            rNum = Math.floor(Math.random() * Math.floor(2));
+            if (rNum == 0) {
+              message.channel.send(
+                "Congratulations, you have passed the vibe test!"
+              );
+            } else if (rNum == 1) {
+              message.channel.send("***You have failed the vibe test***");
+            }
+            targetHasResponded = true;
+          }
+        });
+      }
     }
   },
 };
