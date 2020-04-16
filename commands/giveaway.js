6ff6c07ba1862d0bf0ea553c852502a9e7461842
Valentine Briese <controlfreak707@gmail.com>
2020-04-15 21:18:43 -0700

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -90,7 +90,7 @@ module.exports = {
                     );
                     winners.push(peopleReacted[index]);
                     // After adding a user to winners, remove that item from the array to prevent him from winning multiple times
-                    peopleReacted.splice(index, 1);
+                    peopleReacted.cache.splice(index, 1);
                   }
                 }
 
