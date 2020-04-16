ddd99116274e4a079c768080f703b3e70d00fbbf
Valentine Briese <controlfreak707@gmail.com>
2020-04-15 20:18:28 -0700

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -46,9 +46,11 @@ module.exports = {
           }
 
           if (initialClock >= 60) {
-            finishedClock = `${d.getHours() + 1}:${initialClock - 60}`;
+            finishedClock = `${d.getHours() + 1}:${(
+              initialClock - 60
+            ).toString()}`;
           } else {
-            finishedClock = `${d.getHours()}:${initialClock}`;
+            finishedClock = `${d.getHours()}:${initialClock.toString()}`;
           }
 
           let Embed = new MessageEmbed()
