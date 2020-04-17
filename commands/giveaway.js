6118f8c22f9f67149062bbff99f674c7f061df78
Valentine Briese <controlfreak707@gmail.com>
2020-04-16 18:48:33 -0700

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -97,6 +97,7 @@ module.exports = {
                 var winnerMsg = "User(s) ";
                 for (var i = 0; i < winners.length; i++) {
                   // Add each winner to the winnerMsg
+                  console.log(winners[i]);
                   winnerMsg += winners[i].toString() + ", ";
                 }
 
