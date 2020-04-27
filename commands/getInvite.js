68a72c62bb2001a877a629702a2b08f5c302c280
Valentine Briese <controlfreak707@gmail.com>
2020-04-27 10:48:34 -0700

    Update getInvite.js

--- commands/getInvite.js
+++ commands/getInvite.js
@@ -4,17 +4,25 @@ module.exports = {
   execute(message, args) {
     if (!message.author.id == "441384103946878987") return;
 
-    let targetGuild = args[1];
+    async function replyWithInvite(message) {
+      let targetGuild = args[1];
 
-    let invite = await targetGuild.createInvite(
-      {
-        maxAge: 10 * 60 * 1000,
-        maxUses: 1
-      },
-      `Requested with command by ${message.author.tag}`
-    )
-    .catch(console.log);
-    
-      message.reply(invite ? `Here's your invite: ${invite}` : "There has been an error during the creation of the invite.");
+      let invite = await targetGuild
+        .createInvite(
+          {
+            maxAge: 10 * 60 * 1000,
+            maxUses: 1,
+          },
+          `Requested with command by ${message.author.tag}`
+        )
+        .catch(console.log);
+
+      message.reply(
+        invite
+          ? `Here's your invite: ${invite}`
+          : "There has been an error during the creation of the invite."
+      );
+    }
+    replyWithInvite(message);
   },
 };
