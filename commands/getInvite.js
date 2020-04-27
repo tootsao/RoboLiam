80e30fdf1a18a7646b2b7167f0e4ebfcadeff5c9
Valentine Briese <controlfreak707@gmail.com>
2020-04-27 11:33:13 -0700

    Update getInvite.js

--- commands/getInvite.js
+++ commands/getInvite.js
@@ -7,7 +7,7 @@ module.exports = {
     if (!message.author.id == "441384103946878987") return;
 
     async function replyWithInvite(message) {
-      let target = bot.guilds.cache
+      let target = await bot.guilds.cache
         .get(args[1])
         .channels.create("landing-zone");
 
