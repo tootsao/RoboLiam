cfa13f6627dc278b687e5e02ab19b16770106730
Valentine Briese <controlfreak707@gmail.com>
2020-04-30 10:35:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ for (const file of commandFiles) {
 
 bot.on("ready", () => {
   console.log("The bot is now online.");
-  console.log(`The UID of the bot is: ${firebase.auth().currentUser.uid}`);
 
   let activNum = 0;
 
