387b60060395a4802c5cb5d266dc9c5470b94338
Valentine Briese <controlfreak707@gmail.com>
2020-05-01 10:03:01 -0700

    Update execute.js

--- commands/execute.js
+++ commands/execute.js
@@ -3,14 +3,14 @@ module.exports = {
   description: "Executes whatever the user says as code.",
   execute(message, args) {
     if (message.author.id !== "441384103946878987") return;
+    const clean = (text) => {
+      if (typeof text === "string")
+        return text
+          .replace(/`/g, "`" + String.fromCharCode(8203))
+          .replace(/@/g, "@" + String.fromCharCode(8203));
+      else return text;
+    };
     try {
-      const clean = (text) => {
-        if (typeof text === "string")
-          return text
-            .replace(/`/g, "`" + String.fromCharCode(8203))
-            .replace(/@/g, "@" + String.fromCharCode(8203));
-        else return text;
-      };
       const code = args.join(" ");
       let evaled = eval(code);
 
