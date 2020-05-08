6941382fb37b15d7fd42cbdbc94559a85504f72c
Valentine Briese <controlfreak707@gmail.com>
2020-05-08 09:29:54 -0700

    Update summon.js

--- commands/summon.js
+++ commands/summon.js
@@ -14,29 +14,32 @@ module.exports = {
           message.channel.send("I couldn't find the user you requested.")
         );
     }
-    let invite = message.channel
-      .createInvite(
-        {
-          maxAge: 0,
-          maxUses: 1,
-        },
-        `Requested with "summon" command by ${message.author.tag}.`
-      )
-      .catch(() => {
-        console.log;
-        message.channel.send(
-          "There was an error creating the invite, please try again later."
-        );
-      });
-    targetUser
-      .send(
-        `${targetUser}, ${message.author} has summoned you! To accept, click on the following invite!\nhttps://discord.gg/${invite.code}`
-      )
-      .then(message.channel.send("✅ Summon request successfully sent!"))
-      .catch(
-        message.channel.send(
-          "There was an error sending a message to the requested user. They may have their DMs disabled!"
+    async function createInvite(message) {
+      let invite = await message.channel
+        .createInvite(
+          {
+            maxAge: 0,
+            maxUses: 1,
+          },
+          `Requested with "summon" command by ${message.author.tag}.`
         )
-      );
+        .catch(() => {
+          console.log;
+          message.channel.send(
+            "There was an error creating the invite, please try again later."
+          );
+        });
+      targetUser
+        .send(
+          `${targetUser}, ${message.author} has summoned you! To accept, click on the following invite!${invite}`
+        )
+        .then(message.channel.send("✅ Summon request successfully sent!"))
+        .catch(
+          message.channel.send(
+            "There was an error sending a message to the requested user. They may have their DMs disabled!"
+          )
+        );
+    }
+    createInvite(message);
   },
 };
