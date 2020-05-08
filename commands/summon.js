44263d53bfa899a1d5d713b0a2cc4f78b8021af7
Valentine Briese <controlfreak707@gmail.com>
2020-05-08 08:52:45 -0700

    Update summon.js

--- commands/summon.js
+++ commands/summon.js
@@ -4,10 +4,11 @@ module.exports = {
   description: "Summons the requested user.",
   execute(message, args) {
     let targetUser;
+    let targetUsername = args.slice(1).join(" ");
     if (message.mentions.users.first()) {
       targetUser = message.mentions.users.first();
     } else {
-      targetUser = bot.users.cache.find("username", args[1]);
+      targetUser = bot.users.cache.find("username", targetUsername);
     }
     message.channel.send(`targetUser = ${targetUser}`);
   },
