abf8d9ff51aa75c045743eab88d6b5a88b4cd6a5
Valentine Briese <controlfreak707@gmail.com>
2020-05-08 09:45:28 -0700

    Update summon.js

--- commands/summon.js
+++ commands/summon.js
@@ -3,44 +3,48 @@ module.exports = {
   name: "summon",
   description: "Summons the requested user.",
   execute(message, args) {
-    let targetUser;
-    let targetTag = args.slice(1).join(" ");
-    if (message.mentions.users.first()) {
-      targetUser = message.mentions.users.first();
+    if (!args) {
+      message.channel.send("Please specify who to summon.");
     } else {
-      targetUser = bot.users.cache.find((user) => user.tag == targetTag);
-    }
-    if (!targetUser) {
-      message.channel.send("I couldn't find the user you requested.");
-    }
-    async function createInvite(message) {
-      let invite = await message.channel
-        .createInvite(
-          {
-            maxAge: 0,
-            maxUses: 1,
-          },
-          `Requested with "summon" command by ${message.author.tag}.`
-        )
-        .catch(() => {
-          console.log;
-          message.channel.send(
-            "There was an error creating the invite, please try again later."
-          );
-        });
-      targetUser
-        .send(
-          `${targetUser}, ${message.author} has summoned you! To accept, click on the following invite!${invite}`
-        )
-        .then(() => {
-          message.channel.send("✅ Summon request successfully sent!");
-        })
-        .catch(() =>
-          message.channel.send(
-            "There was an error sending a message to the requested user. They may have their DMs disabled!"
+      let targetUser;
+      let targetTag = args.slice(1).join(" ");
+      if (message.mentions.users.first()) {
+        targetUser = message.mentions.users.first();
+      } else {
+        targetUser = bot.users.cache.find((user) => user.tag == targetTag);
+      }
+      if (!targetUser) {
+        message.channel.send("I couldn't find the user you requested.");
+      }
+      async function createInvite(message) {
+        let invite = await message.channel
+          .createInvite(
+            {
+              maxAge: 0,
+              maxUses: 1,
+            },
+            `Requested with "summon" command by ${message.author.tag}.`
           )
-        );
+          .catch(() => {
+            console.log;
+            message.channel.send(
+              "There was an error creating the invite, please try again later."
+            );
+          });
+        targetUser
+          .send(
+            `${targetUser}, ${message.author} has summoned you! To accept, click on the following invite!${invite}`
+          )
+          .then(() => {
+            message.channel.send("✅ Summon request successfully sent!");
+          })
+          .catch(() =>
+            message.channel.send(
+              "There was an error sending a message to the requested user. They may have their DMs disabled!"
+            )
+          );
+      }
+      createInvite(message);
     }
-    createInvite(message);
   },
 };
