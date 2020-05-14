e1990cbb2a8a554c180345621c05dfcd5a9a943d
Valentine Briese <controlfreak707@gmail.com>
2020-05-14 10:34:02 -0700

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -1,5 +1,30 @@
+const fs = require("fs");
+const bot = require("../index.js");
 module.exports = {
   name: "code",
   description: "Shows the code to the specified file.",
-  execute(message, args) {},
+  execute(message, args) {
+    const commandskid = fs
+      .readFileSync(`RoboLiam/commands/${args[1]}.js`)
+      .toString();
+
+    try {
+      if (args[1]) {
+        const options = {
+          method: "POST",
+          body: commandskid,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        message.channel.send("```js\n" + commandskid.substr(0, 1900) + "```");
+      } else {
+        message.channel.send(
+          "Please specify what command to view the code of."
+        );
+      }
+    } catch (e) {
+      return console.log(e);
+    }
+  },
 };
