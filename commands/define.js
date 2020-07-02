e751b76f84fb2f9f08abc779bc8f15bc558ea46e
Valentine Briese <controlfreak707@gmail.com>
2020-07-01 21:02:27 -0700

    Update define.js

--- commands/define.js
+++ commands/define.js
@@ -8,7 +8,7 @@ module.exports = {
       const {
         response,
       } = /*await*/ fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20200702T031014Z.224775ae6b942b5e.d757485e9a8a4540385b78899c7d21a391e6eb93
-      &lang=en-en&text=${args[1]}`).then((response) => response.json());
+      &lang=en-en&text=${args[1]}`).then();
       const Embed = new MessageEmbed()
         .setTitle(response.def.text)
         .addField(
