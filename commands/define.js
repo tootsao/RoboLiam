91f89816ebcd1429d2e8d9a072e1f93b1830f5a3
Valentine Briese <controlfreak707@gmail.com>
2020-07-17 15:14:07 -0700

    Debug define.js

--- commands/define.js
+++ commands/define.js
@@ -3,13 +3,19 @@ module.exports = {
   description:
     "Searches for the word and defines it, providing examples if existant.",
   execute(message, args) {
+    console.log("Command begin"); //remove
     async function AsyncFunc(message, args) {
+      console.log("AsyncFunc begin"); //remove
       if (args[1]) {
+        console.log("args[1] detected"); //remove
         const { MessageEmbed } = require("discord.js");
+        console.log("MessageEmbed defined"); //remove
         const fetch = await require("node-fetch");
+        console.log("fetch defined"); //remove
         const response = await fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20200702T031014Z.224775ae6b942b5e.d757485e9a8a4540385b78899c7d21a391e6eb93
         &lang=en-en&text=${args[1]}`).then((response) => response.json());
-        console.log(response);
+        console.log("response:"); //remove
+        console.log(response); //remove
         const Embed = new MessageEmbed()
           .setTitle(response.def.text)
           .addField(
@@ -17,6 +23,9 @@ module.exports = {
             response.map((response) => response.def.tr.mean)
           )
           .join("\n\n");
+        console.log("Embed defined"); //remove
+        message.channel.send(Embed);
+        console.log("Embed Sent"); //remove
       } else {
         message.channel.send("Please specify a word to define.");
       }
