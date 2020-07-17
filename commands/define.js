87a6f483409bbecc8e0d6be23a354acbda0fccd4
Valentine Briese <controlfreak707@gmail.com>
2020-07-17 15:55:16 -0700

    Update define.js

--- commands/define.js
+++ commands/define.js
@@ -14,16 +14,17 @@ module.exports = {
           const fetch = await require("node-fetch");
           console.log("fetch defined"); //remove
           const response = await fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20200702T031014Z.224775ae6b942b5e.d757485e9a8a4540385b78899c7d21a391e6eb93
-          &lang=en-en&text=${args[1]}`); /*.then((response) => response.json())*/
+          &lang=en-en&text=${args[1]}`).then((response) => response.json());
           console.log("response:"); //remove
           console.log(response); //remove
           const Embed = new MessageEmbed()
-            .setTitle(response.def[0])
+            .setTitle(response.def[0].text)
             .addField(
               "Definition",
-              response.def.map((response) => response.tr[0].mean[0])
-            )
-            .join("\n\n");
+              response.def[0]
+                .map((response) => response.tr[0].mean)
+                .join("\n\n")
+            );
           console.log("Embed defined"); //remove
           message.channel.send(Embed);
           console.log("Embed Sent"); //remove
