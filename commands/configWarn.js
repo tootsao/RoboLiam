c825370093fc2585a22445b2ba8e5df2d71de379
Valentine Briese <controlfreak707@gmail.com>
2020-07-18 14:25:29 -0700

    Update configWarn.js

--- commands/configWarn.js
+++ commands/configWarn.js
@@ -3,6 +3,48 @@ module.exports = {
   description: "Configures the warning system.",
   execute(message, args, db) {
     if (message.member.hasPermission("KICK_MEMBERS")) {
+      if (args[2]) {
+        if (args[1] == "ban" || args[1] == "kick" || args[1] == "mute") {
+          if (typeof args[2] == "number") {
+            db.collection("guilds")
+              .doc(message.guild.id)
+              .update({
+                warnPunish: args[1],
+                warnAmount: args[2],
+              })
+              .then(() => {
+                let punishment;
+                switch (args[1]) {
+                  case "ban":
+                    punishment = "banned";
+                    break;
+                  case "kick":
+                    punishment = "kicked";
+                    break;
+                  case "mute":
+                    punishment = "muted";
+                    break;
+                  default:
+                    punishment = "Not Set";
+                    break;
+                }
+                message.channel.send(
+                  `Members will be ${punishment} after being warned ${args[2]} time(s).`
+                );
+              });
+          } else {
+            message.channel.send("Please set warn amount to a number.");
+          }
+        } else {
+          message.channel.send(
+            'Please set punishment to either "ban", "kick", or "mute".'
+          );
+        }
+      }
+    } else {
+      message.channel.send(
+        'Only members with the "Kick Members" permission may use this command.'
+      );
     }
   },
 };
