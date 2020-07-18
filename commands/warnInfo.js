feea635b22533ac3a72e722099b4847370a6055f
Valentine Briese <controlfreak707@gmail.com>
2020-07-18 13:56:20 -0700

    Update warnInfo.js

--- commands/warnInfo.js
+++ commands/warnInfo.js
@@ -2,39 +2,45 @@ module.exports = {
   name: "warnInfo",
   description: "Displays info regarding the server's warning system.",
   execute(message, args, db) {
-    let punishment;
-    let warnings;
-    db.collection("guilds")
-      .doc(message.guild.id)
-      .get()
-      .then((q) => {
-        if (q.exists) {
-          switch (q.data().warnPunish) {
-            case "ban":
-              punishment = "banned";
-              break;
-            case "kick":
-              punishment = "kicked";
-              break;
-            case "mute":
-              punishment = "muted";
-              break;
-            default:
-              punishment = "Not Set";
-              break;
+    async function asyncFunc(message, db) {
+      let punishment;
+      let warnings;
+      db.collection("guilds")
+        .doc(message.guild.id)
+        .get()
+        .then((q) => {
+          if (q.exists) {
+            switch (q.data().warnPunish) {
+              case "ban":
+                punishment = await "banned";
+                break;
+              case "kick":
+                punishment = await "kicked";
+                break;
+              case "mute":
+                punishment = await "muted";
+                break;
+              default:
+                punishment = await"Not Set";
+                break;
+            }
+            warnings = q.data().warnAmount;
           }
-          warnings = q.data().warnAmount;
-        }
-      });
+        });
 
-    if (punishment == "Not Set") {
-      message.channel.send(
-        "The warning system is not compatible with this server.\nPlease contact <@441384103946878987> to fix this."
-      );
-    } else {
-      message.channel.send(
-        `Members will be ${punishment} after being warned ${warnings} time(s).`
-      );
+      switch (punishment) {
+        case "Not Set":
+          message.channel.send(
+            "The warning system is not compatible with this server.\nPlease contact <@441384103946878987> to fix this."
+          );
+          break;
+        default:
+          message.channel.send(
+            `Members will be ${punishment} after being warned ${warnings} time(s).`
+          );
+          break;
+      }
     }
+    asyncFunc(message, db);
   },
 };
