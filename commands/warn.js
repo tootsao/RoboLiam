504a6d4bfe7d6bd39b3442a06c3f38e4af4a09b3
Valentine Briese <controlfreak707@gmail.com>
2020-07-18 15:50:14 -0700

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -11,87 +11,65 @@ module.exports = {
             db.collection("guilds")
               .doc(message.guild.id)
               .collection("members")
-              .get()
-              .then((q) => {
-                if (q.exists) {
-                  db.collection("guilds")
-                    .doc(message.guild.id)
-                    .collection("members")
-                    .doc(message.mentions.users.first().id)
-                    .set({
-                      warns: warns + 1,
-                    })
-                    .then(() => {
-                      db.collection("guilds")
-                        .doc(message.guild.id)
-                        .get()
-                        .then((guild) => {
+              .doc(message.mentions.users.first().id)
+              .set({
+                warns: warns + 1,
+              })
+              .then(() => {
+                db.collection("guilds")
+                  .doc(message.guild.id)
+                  .get()
+                  .then((guild) => {
+                    db.collection("guilds")
+                      .doc(message.guild.id)
+                      .collection("members")
+                      .doc(message.mentions.users.first().id)
+                      .get()
+                      .then((member) => {
+                        if (member.warns > guild.warnAmount) {
+                          message.channel.send(
+                            `Warned ${message.mentions.users.first().tag}.`
+                          );
+                          switch (guild.warnPunish) {
+                            case "ban":
+                              message.mentions.users
+                                .first()
+                                .send(
+                                  `You have been banned for exceeding the warning limit.`
+                                );
+                              break;
+                            case "kick":
+                              message.mentions.users
+                                .first()
+                                .send(
+                                  `You have been kicked for exceeding the warning limit.`
+                                );
+                              break;
+                            case "mute":
+                              message.mentions.users
+                                .first()
+                                .send(
+                                  `You have been muted for exceeding the warning limit.`
+                                );
+                              break;
+                          }
                           db.collection("guilds")
                             .doc(message.guild.id)
                             .collection("members")
                             .doc(message.mentions.users.first().id)
-                            .get()
-                            .then((member) => {
-                              if (member.warns > guild.warnAmount) {
-                                message.channel.send(
-                                  `Warned ${
-                                    message.mentions.users.first().tag
-                                  }.`
-                                );
-                                switch (guild.warnPunish) {
-                                  case "ban":
-                                    message.mentions.users
-                                      .first()
-                                      .send(
-                                        `You have been banned for exceeding the warning limit.`
-                                      );
-                                    break;
-                                  case "kick":
-                                    message.mentions.users
-                                      .first()
-                                      .send(
-                                        `You have been kicked for exceeding the warning limit.`
-                                      );
-                                    break;
-                                  case "mute":
-                                    message.mentions.users
-                                      .first()
-                                      .send(
-                                        `You have been muted for exceeding the warning limit.`
-                                      );
-                                    break;
-                                }
-                                db.collection("guilds")
-                                  .doc(message.guild.id)
-                                  .collection("members")
-                                  .doc(message.mentions.users.first().id)
-                                  .set({
-                                    warns: 0,
-                                  });
-                              } else {
-                                message.channel.send(
-                                  `Warned ${
-                                    message.mentions.users.first().tag
-                                  }.`
-                                );
-                                message.mentions.users
-                                  .first()
-                                  .send(
-                                    `You have been warned by ${message.author}.`
-                                  );
-                              }
+                            .set({
+                              warns: 0,
                             });
-                        });
-                    });
-                } else {
-                  db.collection("guilds")
-                    .doc(message.guild.id)
-                    .collection("members")
-                    .doc(message.mentions.users.first().id)
-                    .set({
-                      warns: 1,
-                    });
-                }
+                        } else {
+                          message.channel.send(
+                            `Warned ${message.mentions.users.first().tag}.`
+                          );
+                          message.mentions.users
+                            .first()
+                            .send(`You have been warned by ${message.author}.`);
+                        }
+                      });
+                  });
               });
           }
         } else {
