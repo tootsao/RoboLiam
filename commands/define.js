245e490b2d5917162b7d411d1cfbbe2d16aaee9b
Valentine Briese <controlfreak707@gmail.com>
2020-07-17 17:30:18 -0700

    Transfer to Urban Dictionary API

--- commands/define.js
+++ commands/define.js
@@ -3,37 +3,34 @@ module.exports = {
   description:
     "Searches for the word and defines it, providing examples if existant.",
   execute(message, args) {
-    console.log("Command begin"); //remove
     async function AsyncFunc(message, args) {
       try {
-        console.log("AsyncFunc begin"); //remove
         if (args[1]) {
-          console.log("args[1] detected"); //remove
           const { MessageEmbed } = require("discord.js");
-          console.log("MessageEmbed defined"); //remove
+
           const fetch = await require("node-fetch");
-          console.log("fetch defined"); //remove
           const response = await fetch(
-            `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20200702T031014Z.224775ae6b942b5e.d757485e9a8a4540385b78899c7d21a391e6eb93
-          &lang=en-en&text=${args[1]}`,
+            "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=WORD",
             {
-              method: "POST", // or 'PUT'
+              method: "GET",
               headers: {
-                "Content-Type": "application/json",
+                "x-rapidapi-host":
+                  "mashape-community-urban-dictionary.p.rapidapi.com",
+                "x-rapidapi-key":
+                  "5f8e5fd523msh05182ea08acf9ffp18a88fjsneef5bcf8b426",
               },
             }
           ).then((response) => response.json());
-          console.log("response:"); //remove
           console.log(response); //remove
+
           const Embed = new MessageEmbed()
-            .setTitle(response.def[0].text)
-            .addField(
-              "Definition",
-              response.def[0].tr[0].mean.map((def) => def.text).join("\n\n")
-            );
-          console.log("Embed defined"); //remove
+            .setTitle(
+              `[${response.list[0].word}](${response.list[0].permalink})`
+            )
+            .addField("Definition", response.list[0].definition)
+            .addField("Example", response.list[0].example);
+
           message.channel.send(Embed);
-          console.log("Embed Sent"); //remove
         } else {
           message.channel.send("Please specify a word to define.");
         }
@@ -42,33 +39,5 @@ module.exports = {
       }
     }
     AsyncFunc(message, args);
-
-    const exampleJson = {
-      head: {},
-      def: [
-        {
-          text: "time",
-          pos: "noun",
-          tr: [
-            {
-              text: "время",
-              pos: "существительное",
-              syn: [{ text: "раз" }, { text: "тайм" }],
-              mean: [{ text: "timing" }, { text: "fold" }, { text: "half" }],
-              ex: [
-                {
-                  text: "prehistoric time",
-                  tr: [{ text: "доисторическое время" }],
-                },
-                { text: "hundredth time", tr: [{ text: "сотый раз" }] },
-                { text: "time-slot", tr: [{ text: "тайм-слот" }] },
-              ],
-            },
-          ],
-        },
-      ],
-    };
-
-    exampleJson.def[0].tr[0].mean.map((def) => def.text).join("\n\n");
   },
 };
