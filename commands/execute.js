ff2f779474ee9e0f1fe152f1436b356fc3deb345
Valentine Briese <controlfreak707@gmail.com>
2020-08-05 17:04:25 -0700

    Add execute command

---
+++ commands/execute.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix } = require("../config.json");
+module.exports = {
+  name: "execute",
+  description: "Executes whatever code the user specifies.",
+  aliases: ["eval", "run"],
+  usage: "[cmd]",
+  args: true,
+  execute(message, args) {
+    if (message.author.id != "441384103946878987")
+      return message.reply("Only developers of this bot can use that command.");
+    const client = message.client;
+    const clean = (text) => {
+      if (typeof text === "string")
+        return text
+          .replace(/`/g, "`" + String.fromCharCode(8203))
+          .replace(/@/g, "@" + String.fromCharCode(8203));
+      else return text;
+    };
+    try {
+      const code = args.slice(0).join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), { code: "xl" }).then(() => {
+        message.delete().catch(console.error);
+      });
+    } catch (err) {
+      message.channel
+        .send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``)
+        .then(() => {
+          message.delete().catch(console.error);
+        });
+    }
+  },
+};
