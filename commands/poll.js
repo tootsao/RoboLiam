14a28d5bb9798cda37d58c1fa584e09f58482dd1
Valentine Briese <controlfreak707@gmail.com>
2020-08-06 14:41:16 -0700

    Add poll command

---
+++ commands/poll.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "poll",
+  description: "Ask a server a yes or no question.",
+  aliases: ["ask", "question", "query"],
+  usage: "[query]",
+  args: true,
+  guildOnly: true,
+  permission: "MANAGE_CHANNELS",
+  execute(message, args) {
+    if (message.member.nickname) {
+      var pollCreator = message.member.nickname;
+    } else {
+      var pollCreator = message.author.username;
+    }
+    const Embed = new MessageEmbed()
+      .setColor(0xffc300)
+      .setTitle("📋 Poll - By @" + pollCreator)
+      .setDescription(args.slice(0).join(" "))
+      .setThumbnail(message.author.avatarURL())
+      .addField("Key", "👍 = Yes!\n👎 = No!");
+    message.channel.send(Embed).then((messageToReact) => {
+      messageToReact.react("👍");
+      messageToReact.react("👎");
+      message.delete({ timeout: 500 }).catch(console.error);
+    });
+  },
+};
