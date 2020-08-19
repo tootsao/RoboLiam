c7a0bbc776399217d9d456ba819321066e4ad9bf
Valentine Briese <controlfreak707@gmail.com>
2020-08-19 16:34:32 -0700

    v2.7.0

---
+++ commands/boop.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+const globalFunctions = require("../index");
+module.exports = {
+  name: "boop",
+  description: "Boops the requested user.",
+  usage: "[user]",
+  args: true,
+  execute(message, args) {
+    if (!message.mentions.users.first())
+      return message.channel.send("Please mention a user.");
+
+    async function AsyncFunc(message) {
+      if (message.mentions.users.first() == message.author) {
+        message.reply(
+          "You booped yourself. You were succesful, but it wasn't very satisfying..."
+        );
+      } else {
+        const Embed = new MessageEmbed()
+          .setTitle(
+            `${message.author.tag} booped ${
+              message.mentions.users.first().tag
+            }.`
+          )
+          .setImage(await globalFunctions.data.getGif("anime+boop", 25));
+
+        message.channel.send(Embed);
+      }
+    }
+    AsyncFunc(message);
+  },
+};
--- commands/changelog.js
+++ commands/changelog.js
@@ -4,12 +4,13 @@ module.exports = {
   description: "Sends the changelog to the most recent update.",
   execute(message, args) {
     const Embed = new MessageEmbed()
-      .setTitle("**Changelog**\nv2.6.0")
+      .setTitle("**Changelog**\nv2.7.0")
       .setDescription(
-        "```diff\n+ Add LICENSE\n+ Add support for discord.gg API\n+ Add changlog (in case you didn't notice)```"
+        "```diff\n+ Make changelog send on update\n+ Add boop command :3\n+ Display version on help command```"
       )
-      .setFooter("Updated 8/16/2020 PDT")
+      .setFooter("Updated 8/19/2020 PDT")
       .setColor(0xffa500);
+    exports.data = Embed;
     message.channel.send(Embed);
   },
 };
