439c6d26686cf4924d164df4fa2cff06edbdb96d
Valentine Briese <controlfreak707@gmail.com>
2020-08-19 17:36:32 -0700

    Reduce boop randomness

--- commands/changelog.js
+++ commands/changelog.js
@@ -4,10 +4,8 @@ module.exports = {
   description: "Sends the changelog to the most recent update.",
   execute(message, args) {
     const Embed = new MessageEmbed()
-      .setTitle("**Changelog**\nv2.7.1")
-      .setDescription(
-        "```diff\n+ Add boop command :3\n+ Display version on help command\n- Revert changelog sending on update (It was hell to make and didn't work)```"
-      )
+      .setTitle("**Changelog**\nv2.8.0")
+      .setDescription("```diff\n- Reduce boop randomness```")
       .setFooter("Updated 8/19/2020 PDT")
       .setColor(0xffa500);
     message.channel.send(Embed);
